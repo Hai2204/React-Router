c092053f6ae6e1fd481e68bcaecf7bf267d4fe2b
Hai <nguyenvanhai220498@gmail.com>
2020-10-17 22:29:15 +0700

    Hai - first commit

---
+++ react-router/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Home from "./component/Home";
+import About from "./component/About"
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
